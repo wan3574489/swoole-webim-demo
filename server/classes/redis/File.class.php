bce1f996e881021b640965f41c1c05cbfeea6de3
wanzengchuang <907515459@qq.com>
2017-03-08 23:50:32 +0800

    加入Redis数据层次

--- server/classes/redis/File.class.php
+++ server/classes/redis/File.class.php
@@ -5,14 +5,15 @@ class File {
     protected $history = array();
     protected $history_max_size = 100;
     protected $history_write_count = 0;
-	
+	public $client;
+
 	private function __construct() {
 		global $rooms;
-        $this->online_dir = ONLINE_DIR;
-		foreach($rooms as $_k => $_v){
-			$this->checkDir($this->online_dir.$_k.'/', true);
-		}
-    }
+		$this->client = new Redis();
+		$this->client->connect('127.0.0.1', 6379);
+		$this->client->setOption(Redis::OPT_PREFIX, 'swoole-webim:');
+	}
+
 	public static function init(){
 		if(self::$instance instanceof self){
 			return false;
@@ -20,51 +21,62 @@ class File {
 		self::$instance = new self();
 	}
 	
-	public static function clearDir($dir) {
-        $n = 0;
-        if ($dh = opendir($dir)) {
-            while (($file = readdir($dh)) !== false) {
-                if ($file == '.' or $file == '..') {
-                    continue;
-                }
-                if (is_file($dir . $file)) {
-                    unlink($dir . $file);
-                    $n++;
-                }
-                if (is_dir($dir . $file)) {
-                    self::clearDir($dir . $file . '/');
-                    $n++;
-                }
-            }
-        }
-        closedir($dh);
-        return $n;
+	public static function clearDir($key) {
+		self::$instance->client->delete($key);
+        return true;
     }
-	
+
+	public static function getRoomidKey($roomid){
+		return 'Room-'.$roomid;
+	}
+
 	public static function changeUser($oldroomid,$fd,$newroomid){
-		$old = self::$instance->online_dir.$oldroomid.'/'.$fd;
+		/*$old = self::$instance->online_dir.$oldroomid.'/'.$fd;
 		$new = self::$instance->online_dir.$newroomid.'/'.$fd;
 		$return = copy($old,$new); //拷贝到新目录
-		unlink($old); //删除旧目录下的文件
-		return $return;
+		unlink($old); //删除旧目录下的文件*/
+		$oldroomidKey = self::getRoomidKey($oldroomid);
+		$newroomidKey = self::getRoomidKey($newroomid);
+
+		$oldValue = self::$instance->clinet->lGet($oldroomidKey,$fd);
+		self::$instance->clinet->lSet($newroomidKey,$fd,$oldValue);
+
+		self::outRoom($oldroomid,$fd);
+		self::joinRoom($newroomid,$fd);
+
+		return true;
 	}
-	
+
+	public static function joinRoom($room,$fd){
+		self::$instance->clinet->sadd("room-".$room,$fd);
+		self::$instance->clinet->set('fd-'.$fd,$room);
+	}
+
+	public static function outRoom($room,$fd){
+		self::$instance->clinet->sRem("room-".$room,$fd);
+		self::$instance->clinet->delete('fd-'.$fd,$room);
+
+	}
+
+	public static function getRoomUsers($room){
+		return self::$instance->clinet->sMembers($room);
+	}
+
 	public static function checkDir($dir, $clear_file = false) {
-        if (!is_dir($dir)) {
-            if (!mkdir($dir, 0777, true)) {
-                rw_deny:
-                trigger_error("can not read/write dir[".$dir."]", E_ERROR);
-                return;
-            }
-        }  else if ($clear_file) {
+        if ($clear_file) {
            self::clearDir($dir);
         }
     }
+
 	//登录
 	public static function login($roomid,$fd, $info){
-		$flag = @file_put_contents(self::$instance->online_dir.$roomid.'/'.$fd, @serialize($info));
+		$roomidKey = self::getRoomidKey($roomid);
+		$flag = self::$instance->clinet->lSet($roomidKey,$fd,json_decode($info));
+
+		self::joinRoom($roomid,$fd);
 		return $flag;
     }
+
 	/**
 	 * 获取所有房间的在线用户
 	 */
@@ -72,13 +84,13 @@ class File {
 		global $rooms;
 		$online_users = array();
 		foreach($rooms as $_k => $_v){
-			$online_users[$_k] = array_slice(scandir(self::$instance->online_dir.$_k.'/'), 2);
+			$online_users[$_k] = array_slice(self::getRoomUsers($_k), 2);
 		}
         return $online_users;
     }
 	
 	public static function getUsersByRoom($roomid){
-		$users = array_slice(scandir(self::$instance->online_dir.$roomid.'/'), 2);
+		$users = array_slice(self::getRoomUsers($roomid), 2);
 		return $users;
 	}
 	
@@ -92,36 +104,31 @@ class File {
 	
 	public static function getUser($roomid,$userid) {
 		if($roomid == ""){
-			global $rooms;
-			foreach($rooms as $_k => $_v){
-				if(file_exists(self::$instance->online_dir.$_k.'/'.$userid)){
-					$roomid = $_k;
-					break;
-				}
-			}
+			$roomid = self::$instance->clinet->get('fd-'.$userid);
 		}
-        if (!is_file(self::$instance->online_dir.$roomid.'/'.$userid)) {
+        if ($roomid == "") {
             return false;
         }
-        $ret = @file_get_contents(self::$instance->online_dir.$roomid.'/'.$userid);
-        $info = @unserialize($ret);
+
+		$roomidKey = self::getRoomidKey($roomid);
+		$ret = self::$instance->clinet->lGet($roomidKey,$userid);
+        $info = @json_decode($ret,true);
 		$info['roomid'] = $roomid;//赋予用户所在的房间
         return $info;
     }
+
 	public static function logout($userid) {
-		global $rooms;
-		foreach($rooms as $_k => $_v){
-			if(self::exists($_k,$userid)){
-				unlink(self::$instance->online_dir.$_k.'/'.$userid);
-				break;
-			}
-		}
-		
+		$roomid = self::$instance->clinet->get('fd-'.$userid);
+		$roomidKey = self::getRoomidKey($roomid);
+		$flag = self::$instance->clinet->lSet($roomidKey,$userid,0);
+
+		self::outRoom($roomid,$userid);
     }
 	
 	public static function exists($roomid,$userid){
-		if(file_exists(self::$instance->online_dir.$roomid.'/'.$userid)){
-			 return is_file(self::$instance->online_dir.$roomid.'/'.$userid);
+		$roomid = self::$instance->clinet->get('fd-'.$userid);
+		if($roomid == $userid){
+			return true;
 		}
 		return false;
     }
